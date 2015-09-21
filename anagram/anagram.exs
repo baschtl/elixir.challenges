6e6a1c3d006e3af555796020e74d05a6b2220805
Sebastian Oelke <soelke@babbel.com>
2015-09-21 10:44:55 +0200

    Refactoring.

--- anagram/anagram.exs
+++ anagram/anagram.exs
@@ -4,27 +4,29 @@ defmodule Anagram do
   """
   @spec match(String.t, [String.t]) :: [String.t]
   def match(base, candidates) do
-    filtered_candidates = base_not_in_candidates(base, candidates)
+    filtered_candidates = filtered_candidates(base, candidates)
 
-    Enum.flat_map(filtered_candidates, fn(candidate) ->
-      if have_same_length(base, candidate) &&
-         have_same_chars(base, candidate) do
-        [candidate]
-      else
-        []
-      end
-    end)
+    do_match(base, filtered_candidates)
   end
 
-  defp base_not_in_candidates(base, candidates) do
+  defp filtered_candidates(base, candidates) do
     Enum.reject(candidates, fn(candidate) ->
       String.downcase(candidate) == String.downcase(base)
     end)
   end
 
-  defp have_same_length(a, b), do: String.length(a) == String.length(b)
+  defp do_match(base, candidates) do
+    Enum.flat_map(candidates, fn(candidate) ->
+      cond do
+        have_same_length?(base, candidate) && have_same_chars?(base, candidate) -> [candidate]
+        true -> []
+      end
+    end)
+  end
 
-  defp have_same_chars(a, b) do
+  defp have_same_length?(a, b), do: String.length(a) == String.length(b)
+
+  defp have_same_chars?(a, b) do
     chars_of_a = Enum.sort(String.codepoints(String.downcase(a)))
     chars_of_b = Enum.sort(String.codepoints(String.downcase(b)))
 
