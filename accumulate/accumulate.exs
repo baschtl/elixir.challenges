126daf0572a4565f1fd927b24c3df05482417ff8
Sebastian Oelke <sebastian.oelke@solarisbank.de>
2016-06-27 11:39:54 +0200

    Adds accumulate challenge.

---
+++ accumulate/accumulate.exs
@@ -0,0 +1,24 @@
+defmodule Accumulate do
+  @doc """
+    Given a list and a function, apply the function to each list item and
+    replace it with the function's return value.
+
+    Returns a list.
+
+    ## Examples
+
+      iex> Accumulate.accumulate([], fn(x) -> x * 2 end)
+      []
+
+      iex> Accumulate.accumulate([1, 2, 3], fn(x) -> x * 2 end)
+      [2, 4, 6]
+
+  """
+
+  @spec accumulate(list, (any -> any)) :: list
+  def accumulate([h|t], fun) do
+    [fun.(h)|accumulate(t, fun)]
+  end
+
+  def accumulate([], _), do: []
+end
