d62ab9da0267292330b0885dd11baaced14e6fa6
Sebastian Oelke <sebastian.oelke@solarisbank.de>
2016-06-27 11:06:24 +0200

    Adds RNA challenge.

---
+++ rna-transcription/dna.exs
@@ -0,0 +1,21 @@
+defmodule DNA do
+  @dna_to_rna %{
+    ?G => ?C,
+    ?C => ?G,
+    ?T => ?A,
+    ?A => ?U
+  }
+
+  @doc """
+  Transcribes a character list representing DNA nucleotides to RNA
+
+  ## Examples
+
+  iex> DNA.to_rna('ACTG')
+  'UGAC'
+  """
+  @spec to_rna([char]) :: [char]
+  def to_rna(dna) do
+    Enum.map(dna, &(@dna_to_rna[&1]))
+  end
+end
---
+++ rna-transcription/rna_transcription_test.exs
@@ -0,0 +1,30 @@
+if !System.get_env("EXERCISM_TEST_EXAMPLES") do
+  Code.load_file("dna.exs")
+end
+
+ExUnit.start
+ExUnit.configure exclude: :pending, trace: true
+
+defmodule DNATest do
+  use ExUnit.Case
+
+  test "transcribes guanine to cytosine" do
+    assert DNA.to_rna('G') == 'C'
+  end
+
+  test "transcribes cytosine to guanine" do
+    assert DNA.to_rna('C') == 'G'
+  end
+
+  test "transcribes thymidine to adenine" do
+    assert DNA.to_rna('T') == 'A'
+  end
+
+  test "transcribes adenine to uracil" do
+    assert DNA.to_rna('A') == 'U'
+  end
+
+  test "it transcribes all dna nucleotides to rna equivalents" do
+    assert DNA.to_rna('ACGTGGTCTTAA') == 'UGCACCAGAAUU'
+  end
+end
